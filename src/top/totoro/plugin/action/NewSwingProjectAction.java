6dae8901849becc8e829073b629467824ffc2515
totoro-dev <2844150336@qq.com>
2020-08-31 21:15:33 +0800

    [Fix][v1.0.3 优化新建项目时稳定添加相关依赖到pom中]

--- src/top/totoro/plugin/action/NewSwingProjectAction.java
+++ src/top/totoro/plugin/action/NewSwingProjectAction.java
@@ -6,10 +6,16 @@ import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.roots.ui.configuration.ModulesProvider;
 import org.jetbrains.annotations.NotNull;
+import top.totoro.plugin.constant.Constants;
 import top.totoro.plugin.file.Log;
 import top.totoro.plugin.file.SwingProjectInitializer;
 import top.totoro.plugin.ui.NewSwingProjectWrapper;
+import top.totoro.plugin.util.ThreadPoolUtil;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Objects;
 
 public class NewSwingProjectAction extends NewSwingModuleAction {
@@ -22,24 +28,41 @@ public class NewSwingProjectAction extends NewSwingModuleAction {
 
     @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
-        NewSwingProjectWrapper wizard = new NewSwingProjectWrapper((Project) null, ModulesProvider.EMPTY_MODULES_PROVIDER, (String) null);
+        NewSwingProjectWrapper wizard = new NewSwingProjectWrapper(null, ModulesProvider.EMPTY_MODULES_PROVIDER, null);
         NewProjectUtil.createNewProject(wizard);
         Log.d(this, "createNewProject() end");
         String projectPath = wizard.getProjectPath();
-        new Thread(() -> {
+        initProjectFiles(e, projectPath);
+    }
+
+    private void initProjectFiles(AnActionEvent e, String projectPath) {
+        ThreadPoolUtil.execute(() -> {
+            File pomFile = new File(projectPath + "/pom.xml");
+            StringBuilder content = new StringBuilder();
+            FileReader fr = null;
             try {
-                // 新项目的文件都是在子线程中创建的
-                // 延迟一段时间后，防止pom.xml文件未创建
-                Thread.sleep(1000);
-            } catch (InterruptedException exception) {
-                exception.printStackTrace();
+                fr = new FileReader(pomFile);
+                BufferedReader br = new BufferedReader(fr);
+                String line = "";
+                while ((line = br.readLine()) != null) {
+                    content.append(line).append("\n");
+                }
+            } catch (IOException exception) {
+//                exception.printStackTrace();
+            } finally {
+                // 如果依赖还没有写入的话，需要继续等待写入
+                if (!content.toString().contains(Constants.DEPENDENCY)) {
+                    // 新项目的文件都是在子线程中创建的
+                    // 延迟一段时间后，防止pom.xml文件未创建
+                    SwingProjectInitializer.createSwingProjectFiles(projectPath);
+                    // 刷新目录
+                    if (e.getData(CommonDataKeys.VIRTUAL_FILE) != null) {
+                        Objects.requireNonNull(e.getData(CommonDataKeys.VIRTUAL_FILE)).refresh(false, true);
+                    }
+                    initProjectFiles(e, projectPath);
+                }
             }
-            SwingProjectInitializer.createSwingProjectFiles(projectPath);
-            // 刷新目录
-            if (e.getData(CommonDataKeys.VIRTUAL_FILE) != null) {
-                Objects.requireNonNull(e.getData(CommonDataKeys.VIRTUAL_FILE)).refresh(false, true);
-            }
-        }).start();
+        }, 1500);
     }
 
     @Override
