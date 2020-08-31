82fb9f36f7f615876df925db2e4872c0d11bd308
totoro-dev <2844150336@qq.com>
2020-08-31 17:22:52 +0800

    [Fix][v1.0.3 解决修改文件时频繁同步更新的问题，提升性能]

--- src/top/totoro/plugin/core/SimpleFileType.java
+++ src/top/totoro/plugin/core/SimpleFileType.java
@@ -7,17 +7,22 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import top.totoro.plugin.file.Log;
 import top.totoro.plugin.file.SwingResGroupCreator;
+import top.totoro.plugin.util.ThreadPoolUtil;
 
 import javax.swing.*;
 import java.io.File;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.util.Map;
 import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class SimpleFileType extends LanguageFileType {
     private static final String TAG = SimpleFileType.class.getSimpleName();
 
     public static final SimpleFileType INSTANCE = new SimpleFileType();
+    // 解决修改文件时频繁同步更新的问题，提升性能
+    private final Map<VirtualFile, Integer> updateCounts = new ConcurrentHashMap<>();
 
     private SimpleFileType() {
         super(SimpleLanguage.INSTANCE);
@@ -60,12 +65,14 @@ public class SimpleFileType extends LanguageFileType {
         Log.d(TAG, "extractCharsetFromFileContent()");
         if (project == null || virtualFile == null) return super.extractCharsetFromFileContent(project, null, content);
         if (Objects.equals(virtualFile.getExtension(), getDefaultExtension()) && virtualFile.getPath().contains("src/main/resources")) {
-            String swingFilePath = virtualFile.getPath();
-            Log.d(TAG, "swingFilePath : " + swingFilePath);
-            if (swingFilePath.lastIndexOf("src/main") > 0) {
+            int updateCount = updateCounts.computeIfAbsent(virtualFile, key -> 1);
+            updateCounts.put(virtualFile, updateCounts.get(virtualFile) + 1);
+            ThreadPoolUtil.execute(() -> {
+                if (updateCount + 1 != updateCounts.get(virtualFile)) return;
+                String swingFilePath = virtualFile.getPath();
+                Log.d(TAG, "swingFilePath : " + swingFilePath);
                 // 当前处理的资源文件可能是主项目的，也可能是子模块的，需要做定位，决定了R.java文件的准确性
                 String modulePath = swingFilePath.substring(0, swingFilePath.lastIndexOf("src/main"));
-                Log.d(TAG, "project : " + modulePath);
                 // 开始创建或更新资源组，同步修改到R.java中
                 SwingResGroupCreator.createResGroup(modulePath, new File(swingFilePath), content.toString());
                 // 刷新项目，确保修改能第一时间被感应
@@ -74,7 +81,7 @@ public class SimpleFileType extends LanguageFileType {
                     srcFile = srcFile.getParent();
                 }
                 Objects.requireNonNull(srcFile).refresh(true, true);
-            }
+            }, 1000 /* 一秒内的所有修改只同步最后一次的修改 */);
         }
         return super.extractCharsetFromFileContent(project, virtualFile, content);
     }
